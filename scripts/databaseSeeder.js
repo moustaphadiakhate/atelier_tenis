d489027ed5f97dc5132cb3d311c33df5c68cd5a2
Moustapha <moustaphadiakhate1212@gmail.com>
2022-05-16 13:35:56 +0000

    [04] add script for to seed database

---
+++ scripts/databaseSeeder.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import { createRequire } from 'module';
+import configs from '../config.js';
+import Player from '../database/models/player.js';
+
+const require = createRequire(import.meta.url);
+const dataset = require('../data/dataset.json');
+
+console.log(dataset.players[0]);
+mongoose.connect(configs.DBURL, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connection open to seed: ${configs.DBURL}`);
+  mongoose.connection.db.dropCollection('players', () => {
+    dataset.players.forEach(async (p, index) => {
+      const player = new Player(p);
+      await player.save(() => {
+        if (index === dataset.players.length - 1) {
+          console.log(`Mongoose connection closed seeding : ${configs.DBURL} finised`);
+          mongoose.disconnect();
+        }
+      });
+    });
+  });
+});
