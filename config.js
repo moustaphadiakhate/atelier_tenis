90aa889d54138cadf15d7b10ca98a8cce285ac38
Moustapha <moustaphadiakhate1212@gmail.com>
2022-05-16 15:54:56 +0000

    [06] Let's add process listners
    for uncaughtException _ unhandledRejection _SIGINT _ SIGTERM _ SIGINT events received

--- config.js
+++ config.js
@@ -2,6 +2,48 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+async function envenHandler({ code, message }) {
+  if (code === 0) {
+    console.error(message);
+    // TODO : wait some seconds (send alerts to our ALTELIER SERVERS alert HOOKS) before EXIT
+    process.exit(code);
+  }
+}
+process.on('uncaughtException', (err) => {
+  const event = {
+    code: 1,
+    message: `\n\n ${err.message}\n ${err.stack}`,
+  };
+  envenHandler(event);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled rejection at promise:');
+  console.error(promise);
+  console.error(reason.stack);
+});
+
+process.on('SIGINT', () => {
+  envenHandler({
+    code: 0,
+    message: 'SIGINT received',
+  });
+});
+
+process.on('SIGTERM', () => {
+  envenHandler({
+    code: 0,
+    message: 'SIGTERM received',
+  });
+});
+
+process.on('SIGINT', () => {
+  envenHandler({
+    code: 0,
+    message: 'SIGINT received',
+  });
+});
+
 const configs = {
   env: process.env.NODE_ENV,
   PORT: process.env.PORT,
